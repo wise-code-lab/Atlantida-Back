def60900395940f6453faef7e671bd8c6972244d
bav2212 <bav2212@gmail.com>
2019-03-21 18:41:34 +0200

    add role seeder, which upload all roles before app starts (id needed) #htxkf

--- Atlantida/Program.cs
+++ Atlantida/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Atlantida.Extensions;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +15,7 @@ namespace Atlantida
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().SeedData().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
